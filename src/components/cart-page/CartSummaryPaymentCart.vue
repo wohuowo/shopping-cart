929543c0c662cc4f60916c758f39796e780c889a
Tony Obriku <tonyobriku@gmail.com>
2022-04-29 01:37:38 +0100

    finished

---
+++ src/components/cart-page/CartSummaryPaymentCart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cart-item-card">
+    <h3>Cart Total: ${{ cart_total.toFixed(2) }}</h3>
+    <button class="view-product-button">Pay by Credit Card</button>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    cart_total() {
+      return this.$store.getters.cartTotal;
+    },
+  },
+};
+</script>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,9 +1,27 @@
 <template>
-  <p>hhhh</p>
+  <div>
+    <h1>Your Cart</h1>
+
+    <CartItemCard
+      v-for="product in items"
+      :key="product.id"
+      :product="product"
+    />
+    <CartSummaryPaymentCart />
+  </div>
 </template>
 
 <script>
-export default {};
+import CartItemCard from "../components/cart-page/CartItemCard.vue";
+import CartSummaryPaymentCart from "../components/cart-page/CartSummaryPaymentCart.vue";
+export default {
+  components: { CartItemCard, CartSummaryPaymentCart },
+  computed: {
+    items() {
+      return this.$store.getters.cartItems;
+    },
+  },
+};
 </script>
 
 <style>
